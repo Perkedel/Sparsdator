46c6071751eee7ec246f5627f498fc7f42f0b0a6
Joel Robert Justiawan <JOELwindows7@users.noreply.github.com>
2022-02-28 22:52:37 +0700

    okay here
    we made the stuff easyer

--- ShellScripts/setup_and_update.sh
+++ ShellScripts/setup_and_update.sh
@@ -1,2 +1,4 @@
 sudo apt update
 sudo apt upgrade -y
+sudo apt install build-essential gcc g++ clang -y
+sudo apt install python pkg-config libboost-dev libfreetype6-dev libx11-dev libxext-dev libxrandr-dev libxcomposite-dev libxinerama-dev libxcursor-dev libasound2-dev lv2-dev liblilv-dev libsuil-dev ladspa-sdk libcurl4-openssl-dev fonts-roboto -y
