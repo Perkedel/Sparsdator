ccd9938c6a9ac403b14f9f3e4768a156221d3bcb
Joel Robert Justiawan <JOELwindows7@users.noreply.github.com>
2022-03-01 17:31:11 +0700

    okay, only libc6
    that's it.

--- ShellScripts/setup_and_update.sh
+++ ShellScripts/setup_and_update.sh
@@ -3,4 +3,4 @@ sudo apt upgrade -y
 sudo apt install build-essential gcc g++ clang -y
 sudo apt install python pkg-config libboost-dev libfreetype6-dev libx11-dev libxext-dev libxrandr-dev libxcomposite-dev libxinerama-dev libxcursor-dev libasound2-dev lv2-dev liblilv-dev libsuil-dev ladspa-sdk libcurl4-openssl-dev fonts-roboto -y
 sudo apt install zip p7zip-full -y # https://www.linuxfordevices.com/tutorials/ubuntu/7zip-files-on-ubuntu
-sudo apt install libc++ libc6 -y
+sudo apt install libc6 -y
