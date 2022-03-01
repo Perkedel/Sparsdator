e11fce04873bd665068688f7af7868c5e54c698f
Joel Robert Justiawan <JOELwindows7@users.noreply.github.com>
2022-03-02 00:56:25 +0700

    begging
    ```
    Run cd ~/SparsdatField/ardour
    Setting top to                           : /home/runner/SparsdatField/ardour
    Setting out to                           : /home/runner/SparsdatField/ardour/build
    Checking for 'gcc' (C compiler)          : /usr/bin/gcc
    Checking for 'g++' (C++ compiler)        : /usr/bin/g++
    
    Global Configuration
     * Install prefix                                    : /usr/local
     * Debuggable build                                  : True
     * Build documentation                               : False
    
    Ardour Configuration
     * Will build against private GTK dependency stack   : no
     * Will rely on libintl built into libc              : yes
     * Will build against private Ardour dependency stack : no
    Checking for function 'dlopen' in dlfcn.h            : yes
    Checking for boost library >= 1.56                   : ok
    Checking for program 'pkg-config'                    : /usr/bin/pkg-config
    Checking for 'alsa'                                  : yes
    Checking for 'libpulse'                              : not found
    Checking for 'glib-2.0' >= 2.28                      : yes
    Checking for 'glib-2.0' >= 2.64                      : yes
    Checking for 'gthread-2.0' >= 2.2                    : yes
    Checking for 'glibmm-2.4' >= 2.32.0                  : not found
    The configuration failed
    (complete log in /home/runner/SparsdatField/ardour/build/config.log)
    ```

--- ShellScripts/setup_and_update.sh
+++ ShellScripts/setup_and_update.sh
@@ -3,7 +3,7 @@ sudo apt upgrade -y
 sudo apt install build-essential gcc g++ clang -y
 sudo apt install python pkg-config libboost-dev libfreetype6-dev libx11-dev libxext-dev libxrandr-dev libxcomposite-dev libxinerama-dev libxcursor-dev libasound2-dev lv2-dev liblilv-dev libsuil-dev ladspa-sdk libcurl4-openssl-dev fonts-roboto -y
 sudo apt install zip p7zip-full -y # https://www.linuxfordevices.com/tutorials/ubuntu/7zip-files-on-ubuntu
-sudo apt install libc6 libjack-dev -y
+sudo apt install libc6 libjack-dev libpulse-dev -y
 sudo apt install lua lua-ansicolors lua-any lua-apr lua-apr-dev lua-apr-dev lua-augeas lua-axtlsl lua-basexx lua-bitop lua-bitop-dev lua-cgi lua-busted lua-check lua-cjson-dev lua-cliargs lua-compat53 lua-copas lua-cosmo lua-coxpcall lua-cqueues lua-curl lua-cqueues-dev lua-curl-dev lua-curses lua-doc lua-filesystem lua-filesystem-dev lua-http  -y
 # ah so long
 # You need OpenGL development packages too!
@@ -14,3 +14,5 @@ sudo apt-get install libao-dev libmpg123-dev -y
 # https://stackoverflow.com/a/44390021/9079640
 # https://askubuntu.com/a/705385/64076
 sudo apt-get install binutils-mingw-w64 binutils-mingw-w64-i686 binutils-mingw-w64-x86-64 gcc-mingw-w64 gcc-mingw-w64-i686 gcc-mingw-w64-x86-64 g++-mingw-w64 mingw-w64 g++-mingw-w64-x86-64 g++-mingw-w64-i686 mingw-w64-common mingw-w64-x86-64-dev -y
+# https://ubuntuforums.org/showthread.php?t=2305832
+sudo apt-get install libglibmm-2.4-dev libglibmm-2.4-1v5 libgtkglext1-dev libgtkglextmm-x11-1.2-dev -y
